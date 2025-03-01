f1689582163c89a8d2d1be1bc479b84c60f4c3ae
hassan9494 <hassanalwan36@gmail.com>
2025-03-01 16:14:24 +0300

    send mail

--- Modules/Shop/Http/Controllers/Api/OrderController.php
+++ Modules/Shop/Http/Controllers/Api/OrderController.php
@@ -83,7 +83,7 @@ class OrderController extends Controller
         $details = [
             'subject' => 'Your Microelectron Order has been received',
         ];
-//        Mail::to($user->email)->send(new SendOrderDetailsEmail($details,$order));
+        Mail::to($user->email)->send(new SendOrderDetailsEmail($details,$order));
 
         return new OrderResource($order);
     }
@@ -112,7 +112,7 @@ class OrderController extends Controller
             'subject' => 'Your Microelectron Order has been received',
         ];
 
-//        Mail::to($request->customer['email'])->send(new SendOrderDetailsEmail($details,$order));
+        Mail::to($request->customer['email'])->send(new SendOrderDetailsEmail($details,$order));
 
         return new OrderResource($order);
     }
