627cde4716b47a7ea8f6834d80641f8c7192367c
hassan9494 <hassanalwan36@gmail.com>
2025-04-20 20:21:44 +0300

    add uuid and qr to order and some fixes

--- Modules/Admin/Http/Controllers/Api/OrderController.php
+++ Modules/Admin/Http/Controllers/Api/OrderController.php
@@ -286,10 +286,10 @@ class OrderController extends ApiAdminController
 //            ], 422);
 //        }
 
-//        return response()->json([
-//            'status' => 'success',
-//            'invoice_id' => $xml
-//        ]);
+        return response()->json([
+            'status' => 'success',
+            'invoice_id' => $xml
+        ]);
 
 
 
