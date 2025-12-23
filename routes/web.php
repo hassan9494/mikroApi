d5cff5b4088069a0f6c20a39ef35dffd617335ef
hassan9494 <hassanalwan36@gmail.com>
2025-12-23 18:38:30 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -384,7 +384,7 @@ Route::get('/test-custom-search', function() {
     return response()->json($response);
 });
 Route::get('/convertarraytoboolproduct', function() {
-    $products = \Modules\Shop\Entities\Product::all();
+    $products = \Modules\Shop\Entities\Product::where('id' ,'<=',6506)->get();
     foreach ($products as $product){
 
         $product->update([
